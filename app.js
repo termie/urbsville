a687cf84eded8e6f6494fc65769c8fd8bb10cc9e
andy <github@anarkystic.com>
2010-08-20 13:35:19 +0200

    working again without express

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+require.paths.unshift('./third_party/');
+require.paths.unshift('./public/js');
 require.paths.unshift('./third_party/node-static/lib/');
 require.paths.unshift('./third_party/node.routes.js/');
 require.paths.unshift('./third_party/node_mDNS/');
-require.paths.unshift('./third_party/');
-require.paths.unshift('./public/js');
 require.paths.unshift('./lib');
 var http = require('http');
 var sys = require('sys');
@@ -22,18 +22,18 @@ var routes = require('routes');
  * 3. Announce itself on the local network
  */
 
-var fileServer = static.Server('./public');
-var viewServer = static.Server('./views');
+var fileServer = new static.Server('./public');
+var viewServer = new static.Server('./views');
 
 function render_admin(request, response) {
-  viewServer.serveFile('admin.html', request, response);
+  viewServer.serveFile('admin.html', 200, {}, request, response);
 }
 function render_device(request, response) {
-  viewServer.serveFile('device.html', request, response);
+  viewServer.serveFile('device.html', 200, {}, request, response);
 }
 
 function render_static(request, response, file) {
-  fileServer.serveFile(file, request, response);
+  fileServer.serveFile(file, 200, {}, request, response);
 }
 
 var urls = [
@@ -44,9 +44,7 @@ var urls = [
 
 
 var webServer = http.createServer(function (request, response) {
-  request.addListener('end', function () {
-    routes.route(request, response, urls);
-  });
+  routes.route(request, response, urls);
 }).listen(8000);
 
 var hub = new urb.Urb('Urb', 'hub');
