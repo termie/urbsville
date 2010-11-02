8d95f9ec5c34ee30532bd5fd5d602707e930d60f
andy <github@anarkystic.com>
2010-11-02 22:21:44 +0900

    Got the TCP protocol pieces working again
    Still needs tests to be updated for it.

--- client.js
+++ client.js
@@ -1,29 +1,18 @@
-require.paths.unshift('./third_party/express/lib/');
-require.paths.unshift('./third_party/node_mDNS/');
-require.paths.unshift('./public/js');
-require.paths.unshift('./third_party/Socket.IO-node/lib');
+require.paths.unshift('./third_party/');
+require.paths.unshift('./third_party/node_mdns/lib');
+require.paths.unshift('./third_party/node-dojo/');
 require.paths.unshift('./lib');
-require('express');
-require('express/plugins');
 
-
-var http = require('http');
-var sys = require('sys');
 var urb = require('urb');
-var urb_node = require('urb-node');
-var io = require('socket.io');
+var tcp = require('urb/protocol/tcp');
+var api = require('urb/api');
 
-var tcpApiClient = new urb.ApiClient('ApiClient', 'tcp');
-var tcpTransport = new urb_node.TcpClientTransport(9001, '127.0.0.1');
+var tcpApiProtocol = new tcp.TcpClientProtocol(9001, '127.0.0.1');
+var tcpApiClient  = new api.ApiClient('tcp', tcpApiProtocol);
 
-tcpApiClient.addListener(new urb.Listener(/.*/, function (event) { 
-    sys.puts('tcp ' + sys.inspect(event)); }));
-//hub.addListener(new urb.Listener(/.*/, function (event) {
-//    sys.puts('hub ' + sys.inspect(event)); }));
-//dev.addListener(new urb.Listener(/.*/, function (event) {
-//    sys.puts('dev ' + sys.inspect(event)); }));
+tcpApiClient.on('event', function (event) { console.log(event) });
 
-tcpApiClient.connect(tcpTransport);
+tcpApiClient.connect();
 
 //setInterval(function () {
 //    if (dev.getProperty('state')) {
